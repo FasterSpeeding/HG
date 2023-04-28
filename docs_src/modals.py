c69504d8e974e423caf2eb2fad612eafe9828426
FasterSpeeding <lucina@lmbyrne.dev>
2023-04-28 18:04:58 +0100

    Update modals.py

--- docs_src/modals.py
+++ docs_src/modals.py
@@ -9,6 +9,8 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with this software.
 # If not, see <https://creativecommons.org/publicdomain/zero/1.0/>.
 
+# pyright: reportIncompatibleMethodOverride=warning
+
 import alluka
 import hikari
 import tanjun
